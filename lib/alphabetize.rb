e744c2126f2691f36344b3d567a2dd16ba792389
Kyle Houghton <kylebenz@hotmail.com>
2019-01-02 23:42:45 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,7 +4,7 @@ require 'pry'
 def alphabetize(arr)
   esper_array = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
   #arr.sort
-  arr.sort_by {|element| alpha.split('').index(element)}
+  arr.sort_by {|element| esper_array.index(element)}
 end
 
 binding.pry
